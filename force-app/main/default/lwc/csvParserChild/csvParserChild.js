2eee7d3412b15525e8ae5c137e5be575ddc0c84c
Lalit360 <lalitrajput4050@gmail.com>
2024-04-26 12:54:51 +0530

    [GARH-2025]

---
+++ force-app/main/default/lwc/csvParserChild/csvParserChild.js
@@ -0,0 +1,68 @@
+import { LightningElement, wire, api, track } from 'lwc';
+
+export default class CsvParserChild extends LightningElement {
+    @api sobjectDetails;
+    fileData;
+    isTableVisibleChild = false;
+    fileLineData = [];
+    fileHeader = [];
+
+    openfileUpload(event) {
+        this.handleClear();
+        const file = event.target.files[0];
+        var reader = new FileReader()
+        reader.onload = () => {
+            var base64 = reader.result.split(',')[1]
+            this.fileData = {
+                'filename': file.name,
+                'base64': base64,
+                'mimeType': file.type
+            }
+        }
+        reader.readAsDataURL(file)
+        this.read(file);
+    }
+
+    async read(file) {
+        try {
+          const result = await this.load(file);
+          this.csvParse(result);
+        } catch (error) {
+          this.dispatchEvent(
+            this.showToastPopup('Error', error, 'error')
+        );
+        }
+      }
+    async load(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+    
+          reader.onload = () => {
+            resolve(reader.result);
+          };
+          reader.onerror = () => {
+            reject(reader.error);
+          };
+          reader.readAsText(file);
+        });
+      }
+      csvParse(csv){
+        if (this.fileData.filename.split('.').pop()=='csv') {
+          const lines = csv.split(/\r\n|\n/);
+          this.fileHeader = lines[0].split(',');
+          for (let index = 1; index < lines.length-1; index++) {
+              const element = lines[index].split(',');
+              if (element) {
+                  this.fileLineData.push(element);
+              }
+          }
+          this.isTableVisibleChild = true;
+        }
+    }
+    handleClear(){
+    this.fileData = {};
+    this.fileLineData = [];
+    this.fileHeader = [];
+    this.isTableVisibleChild = false;
+    }
+}
